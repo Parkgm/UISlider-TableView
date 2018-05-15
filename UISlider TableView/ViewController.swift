0721f90d84c5f96bb6b8ac00855a5bcfcaf54dd5
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-05-15 15:36:34 +0900

    UISlider

--- UISlider TableView/ViewController.swift
+++ UISlider TableView/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sliderMoved(_ sender: Any) {
+        // TableView의 데이터를 새로 뿌려줌
+        
+        myTableView.reloadData()
+        
+        let val = Int(mySlider.value)
+        print(val)
+        print("슬라이드 움직임")
+        myLabel.text = String(val)
+    }
+    
+    //필수적인 delegate 함수 구현
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        cell.textLabel?.text = String(Int(mySlider.value) + indexPath.row)
+        
+        return cell
+        
+    }
+    
 }
+
+
+
+
+
 
